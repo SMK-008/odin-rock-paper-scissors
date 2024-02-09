8c4095ae99f2a5d591897bb8b4e66263d2636eab
SMK-008 <sammy.kitonga@strathmore.edu>
2024-02-09 17:00:38 +0300

    created variables for player & computer selections

---
+++ script.js
@@ -0,0 +1,11 @@
+function playGame(){
+    let mySelection=prompt("Choose between rock ,paper and scissors ")
+    let choices=["Rock","Paper","Scissors"]
+    let index=Math.floor(Math.random()*choices.length)
+    let compSelection=choices[index]
+    
+
+    
+    
+}
+playGame()
